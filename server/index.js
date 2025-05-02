7534290d3f33f96e7c6856483218df4b8960df25
Manish Y M <manishym946@gmail.com>
2025-05-02 16:55:43 +0530

    ik ill get conflicts now

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
-
-server.listen(1600,()=>{
-    console.log("Server is running on port 1600");
-});
+import express from 'express';
+import  cors from 'cors';
+import cookieParser from 'cookie-parser';
+import '.dotenv/config';
+const app = express();
+const PORT = process.env.PORT || 1600;
